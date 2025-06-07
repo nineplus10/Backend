ebf095e205aa4e1822ca0d461a6f102dbbb674aa
Allen <valternatean13@gmail.com>
2025-06-07 14:26:19 +0700

    ref(account): make main router into its own class
    This allows injection of modules that would only have a single instance
    within the entire application, such as DB connection

--- src/account/routes/index.ts
+++ src/account/routes/index.ts
@@ -1,4 +1,4 @@
-import express, { Response } from "express";
+import express, { Response, Router } from "express";
 import {AuthRouter } from "./auth";
 import { AuthController } from "account/controllers/authController";
 import { PrismaPlayer } from "account/repositories/prisma/prismaPlayer";
@@ -13,33 +13,41 @@ import { AuthValidator } from "_lib/Middlewares/AuthValidator";
 import { ValkeySession } from "account/repositories/valkey/valkeySession";
 import { Valkey } from "_lib/Persistence/Valkey";
 
-const vk = new Valkey() // TODO: refactor
-const upAt = Date.now()
-const tokenParser = new BearerParser()
-const authValidator = new AuthValidator(new Jwt("access", tokenParser))
+export class AccountRouterV1 {
+    private readonly _router: Router
+    private readonly _upAt: number
 
-const playerRepo = new PrismaPlayer()
-const sessionCache = new ValkeySession(vk.conn)
+    constructor( vk: Valkey) {
+        this._upAt = Date.now()
 
-const authService = new AuthService(
-                        playerRepo, 
-                        sessionCache,
-                        new Bcrypt(), 
-                        new Jwt("access", tokenParser),
-                        new Jwt("refresh", tokenParser))
-const profileService = new ProfileService(playerRepo)
+        const tokenParser = new BearerParser()
+        const authValidator = new AuthValidator(new Jwt("access", tokenParser))
 
-const authController = new AuthController(authService)
-const profileController = new ProfileController(profileService)
+        const playerRepo = new PrismaPlayer()
+        const sessionCache = new ValkeySession(vk.conn)
 
-const profileRouter = new ProfileRouter(profileController, authValidator)
-const authRouter = new AuthRouter(authController, authValidator)
-const v1AccountRouter = express.Router()
-v1AccountRouter.use("/auth", authRouter.router)
-v1AccountRouter.use("/profile", profileRouter.router)
-v1AccountRouter.use("/health", (_, res: Response, __) => { 
-    res.status(200)
-        .send({ uptime: `${(Date.now() - upAt)/1000}s` })
-})
+        const authService = new AuthService(
+                                playerRepo, 
+                                sessionCache,
+                                new Bcrypt(), 
+                                new Jwt("access", tokenParser),
+                                new Jwt("refresh", tokenParser))
+        const profileService = new ProfileService(playerRepo)
 
-export {v1AccountRouter};
+        const authController = new AuthController(authService)
+        const profileController = new ProfileController(profileService)
+
+        const profileRouter = new ProfileRouter(profileController, authValidator)
+        const authRouter = new AuthRouter(authController, authValidator)
+
+        this._router = express.Router()
+        this._router.use("/auth", authRouter.router)
+        this._router.use("/profile", profileRouter.router)
+        this._router.use("/health", (_, res: Response, __) => { 
+            res.status(200)
+                .send({ uptime: `${(Date.now() - this._upAt)/1000}s` })
+        })
+    }
+
+    get router(): Router {return this._router}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import { ErrorHandler } from "_lib/Middlewares/ErrorHandler";
 import { Logger } from "_lib/Middlewares/Logger";
-import { v1AccountRouter } from "account/routes";
+import { Valkey } from "_lib/Persistence/Valkey";
+import { AccountRouterV1 } from "account/routes";
 import { gEnv } from "env";
 import express, {Response } from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 
 const upAt = Date.now()
+const vkConn = new Valkey()
 const loggerMiddleware = new Logger()
 const errorHandler = new ErrorHandler()
 
+const accountRouter = new AccountRouterV1(vkConn)
 const xpress = express()
+
 xpress.use(loggerMiddleware.handle.bind(loggerMiddleware))
 xpress.use(express.urlencoded({extended: false}))
-xpress.use("/api/account/v1", v1AccountRouter)
+xpress.use("/api/account/v1", accountRouter.router)
 xpress.use("/health", (_, res: Response, __) => { 
     res.status(200)
         .send({ uptime: `${(Date.now() - upAt)/1000}s` })
