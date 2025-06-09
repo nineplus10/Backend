a60743c76ab4b1bcb1bb44c990a13f7447ee2445
Allen <valternatean13@gmail.com>
2025-06-09 10:06:10 +0700

    ref(sys/websocket): move socket.io into its own class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { ErrorHandler } from "_lib/Middlewares/ErrorHandler";
 import { Logger } from "_lib/Middlewares/Logger";
 import { Valkey } from "_lib/Persistence/Valkey";
+import { SocketIoApp } from "_lib/Websocket/socketio";
 import { AccountRouterV1 } from "account/routes";
 import { gEnv } from "env";
 import express, {Response } from "express";
-import { createServer } from "node:http";
-import { Server } from "socket.io";
 
 const upAt = Date.now()
 const vkConn = new Valkey()
@@ -26,12 +25,7 @@ xpress.use("/health", (_, res: Response, __) => {
 xpress.use("/", (_, res: Response, __) => { res.status(404).send() })
 xpress.use(errorHandler.handle.bind(errorHandler))
 
-const srv = createServer(xpress)
-const ws = new Server(srv)
-ws.on("connection", () => {
-    console.log("[WBS] Somebody has joined the connection!")
-})
-
-srv.listen(gEnv.PORT, () => {
+const srv = new SocketIoApp(xpress)
+srv.server.listen(gEnv.PORT, () => {
     console.log(`Up and running on ${gEnv.PORT}`)
 })
