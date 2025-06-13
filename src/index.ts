7eb21e5b383a6e78015f5c9d64b004ad7f011985
Allen <valternatean13@gmail.com>
2025-06-13 20:54:28 +0700

    ref(account): move application to its own class
    Just found out that `app.listen` is not blocking. We could use this to
    simulate modules running on different endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,8 @@
-import { ErrorHandler } from "_lib/Middlewares/ErrorHandler";
-import { Logger } from "_lib/Middlewares/Logger";
-import { Valkey } from "_lib/Persistence/Valkey";
-import { AccountRouterV1 } from "account/routes";
+import { AccountModule } from "account/app";
 import { gEnv } from "env";
-import express, {Response } from "express";
-import cors from "cors"
 
-const upAt = Date.now()
-const vkConn = new Valkey()
-const loggerMiddleware = new Logger()
-const errorHandler = new ErrorHandler()
+// Imma call it 'em module for now instead of services
+// as I'm not knowledgeable enough to use this term
+const accountModule = new AccountModule()
 
-const accountRouter = new AccountRouterV1(vkConn)
-const xpress = express()
-
-xpress.use(loggerMiddleware.handle.bind(loggerMiddleware))
-xpress.use(cors({
-    origin: ["http://localhost:8877"],// TODO: Consider moving it to .env, config, or something
-    optionsSuccessStatus: 200 // Legacy support: https://expressjs.com/en/resources/middleware/cors.html#configuring-cors
-}))
-xpress.use(express.urlencoded({extended: false}))
-
-xpress.use("/api/account/v1", accountRouter.router)
-xpress.use("/health", (_, res: Response, __) => { 
-    res.status(200)
-        .send({ uptime: `${(Date.now() - upAt)/1000}s` })
-})
-xpress.use("/", (_, res: Response, __) => { res.status(404).send() })
-xpress.use(errorHandler.handle.bind(errorHandler))
-
-xpress.listen(gEnv.PORT, () => {
-    console.log(`Up and running on ${gEnv.PORT}`)
-})
+accountModule.listen(gEnv.PORT)
