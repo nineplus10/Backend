9b35d793c52e488b98b0c46d69e8a30dc337431f
Allen <valternatean13@gmail.com>
2025-06-27 10:21:47 +0700

    ci: add `account` service dependency to `game`
    `Game` service now depends to `Account` service to ask whether the
    requestor of the websocket connection currently has an active session.

--- src/gameClient/env.ts
+++ src/gameClient/env.ts
@@ -6,12 +6,12 @@ const validator = z.object({
     /**
      * Endpoint for the cache used by this application
      * */
-    CACHE_URL: z.string(),
+    CACHE_URL: z.string().url(),
 
     /**
      * List of the message broker URLs
      * */
-    BROKER_URL: z.array(z.string()),
+    BROKER_URL: z.array(z.string().url()),
 
     /**
      * Exposed topics for the application to receive the messages from
@@ -25,10 +25,13 @@ const validator = z.object({
     */
     BROKER_PUB_TOPICS: z.object({
         matchmaking: z.string()
-    })
+    }),
+
+    AUTH_REFRESH_URL: z.string().url()
 })
 
 const gameEnv = validator.parse({
+    AUTH_REFRESH_URL: `${process.env.MM_ACCOUNT_URL}${process.env.MM_REFRESH_PATH}`,
     CACHE_URL: process.env.MM_CACHE_URL,
     BROKER_URL: ["broker:19092"],
     BROKER_PUB_TOPICS: {
