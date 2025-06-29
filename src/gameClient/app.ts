78aba028c4ce0e07e0cd002e9bf5c9132f9eab89
Allen <valternatean13@gmail.com>
2025-06-29 21:13:52 +0700

    ref(game): delegate bootstrapping to main app
    The main router previously looks as if they're handling both routing and
    bootstrapping (initiation of components needed for route handling)

--- src/gameClient/app.ts
+++ src/gameClient/app.ts
@@ -3,13 +3,35 @@ import { Kafka } from "../_lib/MessageBroker/kafka"
 import { GameClientRouterV1 } from "./routes/app"
 import { gameEnv } from "./env"
 import { WsApp } from "../_lib/Websocket/ws"
+import { ValkeyMatch } from "./repository/valkey/valkeyMatch"
+import { HighestWinsMatchmaker } from "./domain/services/matchmaker/highestWins"
+import { MatchService } from "./services/match"
+import { MatchController } from "./controller/match"
+import { MatchRouter } from "./routes/match"
 
 export class GameClientModule {
     static async start(listenPort: number): Promise<GameClientModule> {
         const kafka = new Kafka( "test", gameEnv.BROKER_URL)
         const valkey = new Valkey(gameEnv.CACHE_URL)
 
-        const router = await GameClientRouterV1.create(valkey.conn, kafka)
+        const matchCache = new ValkeyMatch(valkey.conn)
+    
+        const matchService = new MatchService(matchCache, new HighestWinsMatchmaker())
+
+        const matchController = new MatchController(matchService)
+
+        const matchRouter = new MatchRouter(matchController)
+
+        const matchmakingInterval = 
+            setInterval(async() => {
+                await matchService.matchmake()
+                    .catch(err => {
+                        console.error(err.message)
+                    })
+            }, 3*1000)
+
+        const router = new GameClientRouterV1(matchRouter)
+
         const app = new WsApp(router, gameEnv.AUTH_REFRESH_URL)
         return app.server.listen(listenPort, () => {
             console.log(`[GameClient] Up and running on ${listenPort}`)
--- src/gameClient/routes/app.ts
+++ src/gameClient/routes/app.ts
@@ -1,47 +1,18 @@
-import { MatchController } from "gameClient/controller/match"
 import { findRouting, WsRouter, WsServeFx } from "."
-import { MatchRouter } from "./match"
 import { Message, Response, OnErrorFx } from "_lib/Websocket"
-import { Valkey } from "_lib/Persistence/Valkey"
-import { MatchService } from "gameClient/services/match"
-import { ValkeyMatch } from "gameClient/repository/valkey/valkeyMatch"
-import { MessageBrokerHandler } from "_lib/MessageBroker"
-import { HighestWinsMatchmaker } from "gameClient/domain/services/matchmaker/highestWins"
+import { MatchRouter } from "./match"
 
 export class GameClientRouterV1 implements WsRouter {
-    private constructor(
-        private readonly _serveFx: [string, WsServeFx][]
-    ) {}
-
-    static async create(
-        cacheConn: Valkey["_conn"], 
-        msgBroker: MessageBrokerHandler
-    ): Promise<GameClientRouterV1> {
-        const matchCache = new ValkeyMatch(cacheConn)
-    
-        const matchService = 
-            new MatchService( matchCache, new HighestWinsMatchmaker())
-
-        const matchController = new MatchController(matchService)
-
-        const matchRouter = new MatchRouter(matchController)
-        const serveFx: GameClientRouterV1["_serveFx"] = [
+    readonly _serveFx: [string, WsServeFx][]
+    constructor( matchRouter: MatchRouter) {
+        this._serveFx = [
             ["match", matchRouter.serve.bind(matchRouter)],
         ]
-
-        // setInterval(async() => {
-        //     await matchService.matchmake()
-        //         .catch(err => {
-        //             console.error(err.message)
-        //         })
-        // }, 3*1000)
-        return new GameClientRouterV1(serveFx)
     }
 
     serve(msg: Message, res: Response, onError: OnErrorFx): void {
-        const destination = msg.meta.destination
-        const [matchServeFx, matchLength] = findRouting(destination, this._serveFx)
-        if(!matchServeFx) {
+        const [serveFx, matchLength] = findRouting(msg.meta.destination, this._serveFx)
+        if(!serveFx) {
             res.status("ERR")
                 .reason("Destination not found")
                 .send()
@@ -50,6 +21,6 @@ export class GameClientRouterV1 implements WsRouter {
         msg.meta.destination = 
             (<string>msg.meta.destination)
             .slice(matchLength)
-        matchServeFx( msg, res, onError)
+        serveFx( msg, res, onError)
     }
 }
